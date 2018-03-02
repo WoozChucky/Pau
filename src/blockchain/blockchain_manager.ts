ed0a6e407d4031535c002dec35f5396cb00cd51e
Nuno Silva <gamesflash94@gmail.com>
2018-03-02 19:01:38 +0000

    Added method to generate genesis block & chain

--- src/blockchain/blockchain_manager.ts
+++ src/blockchain/blockchain_manager.ts
@@ -1,8 +1,8 @@
 import {Block} from "../model/block";
 import {Semaphore} from "prex";
-import {getBlockchain} from "./blockchain";
 import {Database} from "../database";
 import { logger } from '../utils/logging';
+import {Transaction} from "../model/transaction";
 
 export type Blockchain = Block[];
 
@@ -28,7 +28,7 @@ export class BlockchainManager {
             .catch(() => {
                 logger.warning('Error loading blockchain from local database. Using genesis block.');
 
-                BlockchainManager.chain = getBlockchain();
+                BlockchainManager.chain = BlockchainManager.getGenesisChain();
             });
 
         BlockchainManager.inited = true;
@@ -128,5 +128,24 @@ export class BlockchainManager {
             .catch(err => {
                 logger.warning(err);
             });
+    }
+
+    private static getGenesisChain() : Blockchain {
+
+        let genesisTransaction : Transaction = {
+            'txIns': [{'signature': '', 'txOutId': '', 'txOutIndex': 0}],
+            'txOuts': [{
+                'address': '04bfcab8722991ae774db48f934ca79cfb7dd991229153b9f732ba5334aafcd8e7266e47076996b55a14bf9913ee3145ce0cfc1372ada8ada74bd287450313534a',
+                'amount': 50
+            }],
+            'id': 'e655f6a5f26dc9b4cac6e46f52336428287759cf81ef5ff10854f69d68f43fa3'
+        };
+
+        let genesisBlock : Block = new Block(
+            0, '91a73664bc84c0baa1fc75ea6e4aa6d1d20c5df664c724e3159aefc2e1186627',
+            '', 1465154705, [genesisTransaction], 0, 0
+        );
+
+        return [genesisBlock];
     }
 }
